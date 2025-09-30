b9a9d26ca9b8bb3ae2dff45572cf30d8be4309a1
Jake Soenneker <jake@soenneker.com>
2025-09-30 13:38:25 -0500

    scale mod

--- src/ScaleType.cs
+++ src/ScaleType.cs
@@ -8,11 +8,11 @@ namespace Soenneker.Quark.Enums;
 [Intellenum<string>]
 public sealed partial class ScaleType
 {
-    public static readonly ScaleType S0 = new("0");
-    public static readonly ScaleType S1 = new("1");
-    public static readonly ScaleType S2 = new("2");
-    public static readonly ScaleType S3 = new("3");
-    public static readonly ScaleType S4 = new("4");
-    public static readonly ScaleType S5 = new("5");
-    public static readonly ScaleType S6 = new("6");
+    public static readonly ScaleType Is0 = new("0");
+    public static readonly ScaleType Is1 = new("1");
+    public static readonly ScaleType Is2 = new("2");
+    public static readonly ScaleType Is3 = new("3");
+    public static readonly ScaleType Is4 = new("4");
+    public static readonly ScaleType Is5 = new("5");
+    public static readonly ScaleType Is6 = new("6");
 }
